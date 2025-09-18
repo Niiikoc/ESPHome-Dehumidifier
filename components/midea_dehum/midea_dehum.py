6f36fefcbcaa6640785213fa81cba320f619e38a
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2025-09-18 17:01:03 +0200

    Update midea_dehum.py

--- components/midea_dehum/midea_dehum.py
+++ components/midea_dehum/midea_dehum.py
@@ -1,24 +1,21 @@
-import esphome.config_validation as cv
 import esphome.codegen as cg
-from esphome.const import CONF_ID, CONF_UART_ID
+import esphome.config_validation as cv
 from esphome.components import uart
-from esphome import core as _core
+from esphome.const import CONF_ID, CONF_UART_ID
 
-# Create namespace matching your C++ class name
-midea_dehum_ns = cg.esphome_ns.namespace('midea_dehum')
-# C++ class name must match exactly: MideaDehumComponent
-MideaDehum = midea_dehum_ns.class_('MideaDehumComponent', cg.Component, uart.UARTDevice)
+# Match your C++ namespace/class
+midea_dehum_ns = cg.esphome_ns.namespace("midea_dehum")
+MideaDehumComponent = midea_dehum_ns.class_("MideaDehumComponent", cg.Component, uart.UARTDevice)
 
-# YAML keys
-CONF_UART = CONF_UART_ID
-
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(MideaDehum),
-    cv.Required(CONF_UART): cv.use_id(uart.UARTComponent),
-}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(MideaDehumComponent),
+        cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
+    }
+).extend(cv.COMPONENT_SCHEMA)
 
 
 def to_code(config):
-    # create the C++ object with the UARTComponent pointer
-    var = cg.new_Pvariable(config[CONF_ID], config[CONF_UART])
-    cg.register_component(var, config)
+    uart_component = yield cg.get_variable(config[CONF_UART_ID])
+    var = cg.new_Pvariable(config[CONF_ID], uart_component)
+    yield cg.register_component(var, config)
