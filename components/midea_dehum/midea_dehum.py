a4ca5ccaa6cd81e8e9ed5db0b90957d10cb23582
Christos Baltatzidis <68458228+Chreece@users.noreply.github.com>
2025-09-28 18:11:10 +0200

    minimal

--- components/midea_dehum/midea_dehum.py
+++ components/midea_dehum/midea_dehum.py
@@ -11,10 +11,11 @@ CONFIG_SCHEMA = cv.Schema(
         cv.GenerateID(): cv.declare_id(MideaDehum),
         cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
     }
-).extend(climate.CLIMATE_SCHEMA).extend(cv.COMPONENT_SCHEMA)
+).extend(cv.COMPONENT_SCHEMA)
 
 
 async def to_code(config):
+    print(">>> midea_dehum.py to_code called!")  # DEBUG
     uart_comp = await cg.get_variable(config[CONF_UART_ID])
     var = cg.new_Pvariable(config[CONF_ID], uart_comp)
     await cg.register_component(var, config)
