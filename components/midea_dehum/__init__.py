60eec322e1a9c712830f1f8a78a402b15f8dcfc8
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2025-10-02 10:14:48 +0200

    Update __init__.py

---
+++ components/midea_dehum/__init__.py
@@ -0,0 +1,18 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import uart
+from esphome.const import CONF_ID
+
+midea_dehum_ns = cg.esphome_ns.namespace('midea_dehum')
+MideaDehumComponent = midea_dehum_ns.class_('MideaDehumComponent', cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(MideaDehumComponent),
+    cv.Required('uart_id'): cv.use_id(uart.UARTComponent),
+})
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+    uart_component = yield cg.get_variable(config['uart_id'])
+    cg.add(var.set_uart(uart_component))
