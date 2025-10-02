053a1c80b6acc00f0f69de2921edb854e0497070
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2025-10-02 19:55:33 +0200

    Update midea_dehum.h

--- components/midea_dehum/midea_dehum.h
+++ components/midea_dehum/midea_dehum.h
@@ -3,6 +3,8 @@
 #include "esphome/core/component.h"
 #include "esphome/components/climate/climate.h"
 #include "esphome/components/uart/uart.h"
+#include "esphome/components/switch/switch.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
 
 namespace esphome {
 namespace midea_dehum {
@@ -11,29 +13,31 @@ class MideaDehumComponent : public climate::Climate,
                             public uart::UARTDevice,
                             public Component {
  public:
-  // Default ctor for codegen (created with new_Pvariable(id))
   MideaDehumComponent() : uart::UARTDevice() {}
-  // Optional convenience ctor
-  explicit MideaDehumComponent(uart::UARTComponent *parent) : uart::UARTDevice(parent) {}
 
-  // Called from Python to_code() to attach the UART component
   void set_uart(uart::UARTComponent *parent) { this->set_uart_parent(parent); }
 
-  // Component lifecycle
+  // Extra setters for Python binding
+  void set_tank_full_sensor(binary_sensor::BinarySensor *s) { this->tank_full_sensor_ = s; }
+  void set_ion_switch(switch_::Switch *s) { this->ion_switch_ = s; }
+  void set_swing_switch(switch_::Switch *s) { this->swing_switch_ = s; }
+
   void setup() override;
   void loop() override;
-
-  // Climate API
   climate::ClimateTraits traits() override;
   void control(const climate::ClimateCall &call) override;
 
  protected:
-  // UART helpers
   void parse_frame_(const std::vector<uint8_t> &frame);
   void send_command_(const std::vector<uint8_t> &cmd);
   static uint8_t checksum_(const std::vector<uint8_t> &bytes);
 
   std::vector<uint8_t> rx_buf_;
+
+  // Entities
+  binary_sensor::BinarySensor *tank_full_sensor_{nullptr};
+  switch_::Switch *ion_switch_{nullptr};
+  switch_::Switch *swing_switch_{nullptr};
 };
 
 }  // namespace midea_dehum
