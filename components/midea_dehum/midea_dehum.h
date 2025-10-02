986f056430e91115e8066d3de1fac46514ef5b8b
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2025-10-02 10:31:46 +0200

    Update midea_dehum.h

--- components/midea_dehum/midea_dehum.h
+++ components/midea_dehum/midea_dehum.h
@@ -1,20 +1,29 @@
 #pragma once
 
-#include "esphome/components/climate/climate.h"
+#include "esphome.h"
 #include "esphome/components/uart/uart.h"
+#include "esphome/components/climate/climate.h"
 
 namespace esphome {
 namespace midea_dehum {
 
-class MideaDehumComponent : public climate::Climate, public uart::UARTDevice {
+class MideaDehumComponent : public climate::Climate, public UARTDevice {
  public:
-  MideaDehumComponent(uart::UARTComponent *parent) : uart::UARTDevice(parent) {}
+  explicit MideaDehumComponent(UARTComponent *parent) : UARTDevice(parent) {}
+
+  void setup() override;
+  void loop() override;
+
+  // Climate API
+  void control(const climate::ClimateCall &call) override;
+  climate::ClimateTraits traits() override;
 
  protected:
-  void control(const climate::ClimateCall &call) override {
-    // Example: just log calls
-    ESP_LOGI("midea_dehum", "Control received");
-  }
+  void parse_frame_(const std::vector<uint8_t> &frame);
+  void send_command_(const std::vector<uint8_t> &cmd);
+  uint8_t checksum_(const std::vector<uint8_t> &bytes);
+
+  std::vector<uint8_t> rx_buf_;
 };
 
 }  // namespace midea_dehum
