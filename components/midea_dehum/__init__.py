5bb9b64e758a3429af49d4199d2800c3892779a5
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2025-10-02 10:25:19 +0200

    Dependencies

--- components/midea_dehum/__init__.py
+++ components/midea_dehum/__init__.py
@@ -3,6 +3,8 @@ import esphome.config_validation as cv
 from esphome.components import uart
 from esphome.const import CONF_ID
 
+DEPENDENCIES = ["uart", "climate"]
+
 midea_dehum_ns = cg.esphome_ns.namespace('midea_dehum')
 MideaDehumComponent = midea_dehum_ns.class_('MideaDehumComponent', cg.Component)
 
@@ -16,3 +18,4 @@ def to_code(config):
     yield cg.register_component(var, config)
     uart_component = yield cg.get_variable(config['uart_id'])
     cg.add(var.set_uart(uart_component))
+
