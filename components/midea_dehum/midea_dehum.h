14572a253b2203890609ce12fe73b11425bb472c
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2025-10-02 20:11:46 +0200

    Update midea_dehum.h

--- components/midea_dehum/midea_dehum.h
+++ components/midea_dehum/midea_dehum.h
@@ -1,28 +1,37 @@
-#pragma once
-
-#include "esphome/core/component.h"
-#include "esphome/components/climate/climate.h"
-#include "esphome/components/uart/uart.h"
 #include "esphome/components/switch/switch.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
 
 namespace esphome {
 namespace midea_dehum {
 
+class MideaDehumComponent;
+
+class IonSwitch : public switch_::Switch {
+ public:
+  explicit IonSwitch(MideaDehumComponent *parent) : parent_(parent) {}
+ protected:
+  void write_state(bool state) override;  // override switch action
+  MideaDehumComponent *parent_;
+};
+
 class MideaDehumComponent : public climate::Climate,
                             public uart::UARTDevice,
                             public Component {
-public:
+ public:
   MideaDehumComponent() : uart::UARTDevice() {}
   explicit MideaDehumComponent(uart::UARTComponent *parent) : uart::UARTDevice(parent) {}
+
   void set_uart(uart::UARTComponent *parent) { this->set_uart_parent(parent); }
   void set_tank_full_sensor(binary_sensor::BinarySensor *s) { this->tank_full_sensor_ = s; }
-  void set_ion_switch(switch_::Switch *s) { this->ion_switch_ = s; }
+  void set_ion_switch(IonSwitch *s) { this->ion_switch_ = s; }
 
   void setup() override;
   void loop() override;
   climate::ClimateTraits traits() override;
   void control(const climate::ClimateCall &call) override;
+
+  // Called by IonSwitch when toggled
+  void set_ion_state(bool state);
 
  protected:
   void parse_frame_(const std::vector<uint8_t> &frame);
@@ -32,7 +41,7 @@ public:
   std::vector<uint8_t> rx_buf_;
 
   binary_sensor::BinarySensor *tank_full_sensor_{nullptr};
-  switch_::Switch *ion_switch_{nullptr};
+  IonSwitch *ion_switch_{nullptr};
 };
 
 }  // namespace midea_dehum
