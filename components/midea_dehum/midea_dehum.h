0996dd3d1e0800424a2880981d9b020dfe54bdad
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2025-10-03 12:19:18 +0200

    Update midea_dehum.h

--- components/midea_dehum/midea_dehum.h
+++ components/midea_dehum/midea_dehum.h
@@ -2,7 +2,7 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/climate/climate.h"
-#include "uart/uart.h"
+#include "esphome/components/uart/uart.h"
 #include "esphome/components/sensor/sensor.h"
 
 namespace esphome {
