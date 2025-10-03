bd74d1f6696960939a4aaa5109ef41499b44140c
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2025-10-03 12:02:53 +0200

    Update __init__.py

--- components/midea_dehum/__init__.py
+++ components/midea_dehum/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart, climate, switch, binary_sensor
+from esphome.components import uart, climate, sensor
 from esphome.const import CONF_ID, CONF_UART_ID
 
 midea_dehum_ns = cg.esphome_ns.namespace("midea_dehum")
@@ -10,17 +10,14 @@ MideaDehum = midea_dehum_ns.class_(
     uart.UARTDevice,
     cg.Component,
 )
-IonSwitch = midea_dehum_ns.class_("IonSwitch", switch.Switch)
 
-CONF_TANK = "tank_full"
-CONF_ION = "ion"
+CONF_ERROR = "error"
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(MideaDehum),
         cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
-        cv.Optional(CONF_TANK): binary_sensor.binary_sensor_schema(),
-        cv.Optional(CONF_ION): switch.switch_schema(IonSwitch),
+        cv.Optional(CONF_ERROR): sensor.sensor_schema(),  # numeric error code sensor
     }
 ).extend(cv.COMPONENT_SCHEMA).extend(climate.climate_schema(MideaDehum))
 
@@ -33,12 +30,6 @@ async def to_code(config):
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
 
-    # Tank binary_sensor
-    if CONF_TANK in config:
-        sens = await binary_sensor.new_binary_sensor(config[CONF_TANK])
-        cg.add(var.set_tank_full_sensor(sens))
-
-    # Ion switch
-    if CONF_ION in config:
-        sw = await switch.new_switch(config[CONF_ION])
-        cg.add(var.set_ion_switch(sw))
+    if CONF_ERROR in config:
+        err = await sensor.new_sensor(config[CONF_ERROR])
+        cg.add(var.set_error_sensor(err))
