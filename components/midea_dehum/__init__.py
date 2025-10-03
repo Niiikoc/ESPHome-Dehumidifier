c15da8929e4a80d1cc11e5e0d81c3195011f4f95
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2025-10-03 12:45:25 +0200

    Update __init__.py

--- components/midea_dehum/__init__.py
+++ components/midea_dehum/__init__.py
@@ -17,14 +17,15 @@ CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(MideaDehum),
         cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
-        cv.Optional(CONF_ERROR): sensor.sensor_schema(),  # numeric error code sensor
+        cv.Optional(CONF_ERROR): sensor.sensor_schema(unit_of_measurement="", accuracy_decimals=0),
     }
 ).extend(cv.COMPONENT_SCHEMA).extend(climate.climate_schema(MideaDehum))
 
 
 async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
     uart_comp = await cg.get_variable(config[CONF_UART_ID])
-    var = cg.new_Pvariable(config[CONF_ID], uart_comp)
+    cg.add(var.set_uart(uart_comp))
 
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
