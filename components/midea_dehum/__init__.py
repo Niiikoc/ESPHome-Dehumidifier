311148baa61589a3238604f0748a63a68b361d3d
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2025-10-03 13:37:07 +0200

    Update __init__.py

--- components/midea_dehum/__init__.py
+++ components/midea_dehum/__init__.py
@@ -6,9 +6,9 @@ from esphome.const import CONF_ID, CONF_UART_ID
 midea_dehum_ns = cg.esphome_ns.namespace("midea_dehum")
 MideaDehum = midea_dehum_ns.class_(
     "MideaDehumComponent",
+    cg.Component,          # keep Component first
     climate.Climate,
     uart.UARTDevice,
-    cg.Component,
 )
 
 CONF_ERROR = "error"
@@ -17,19 +17,18 @@ CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(MideaDehum),
         cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
-        cv.Optional(CONF_ERROR): sensor.sensor_schema(),
+        cv.Optional(CONF_ERROR): sensor.sensor_schema(accuracy_decimals=0),
     }
 ).extend(cv.COMPONENT_SCHEMA).extend(climate.climate_schema(MideaDehum))
 
-
 async def to_code(config):
-    uart_comp = await cg.get_variable(config[CONF_UART_ID])
     var = cg.new_Pvariable(config[CONF_ID])
+    uart_comp = await cg.get_variable(config[CONF_UART_ID])
     cg.add(var.set_uart(uart_comp))
 
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
 
     if CONF_ERROR in config:
-        sens = await sensor.new_sensor(config[CONF_ERROR])
-        cg.add(var.set_error_sensor(sens))
+        err = await sensor.new_sensor(config[CONF_ERROR])
+        cg.add(var.set_error_sensor(err))
