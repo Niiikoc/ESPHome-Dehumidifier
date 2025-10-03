2ce85625e481bed417c036af63d77854138aeefa
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2025-10-03 18:26:02 +0200

    Update midea_dehum.cpp

--- components/midea_dehum/midea_dehum.cpp
+++ components/midea_dehum/midea_dehum.cpp
@@ -54,6 +54,7 @@ void MideaDehumComponent::loop() {
   uint32_t now = millis();
   if (now - last > 5000) {
     last = now;
+    ESP_LOGD(TAG, "UART available = %u", this->available());
     this->request_status_();
   }
 }
