7bab30cae46b2862871cb3b5194d0a99d96e3c6f
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2025-10-03 12:31:39 +0200

    Update midea_dehum.h

--- components/midea_dehum/midea_dehum.h
+++ components/midea_dehum/midea_dehum.h
@@ -12,10 +12,8 @@ class MideaDehumComponent : public climate::Climate,
                             public uart::UARTDevice,
                             public Component {
  public:
-  MideaDehumComponent() : uart::UARTDevice() {}
   explicit MideaDehumComponent(uart::UARTComponent *parent) : uart::UARTDevice(parent) {}
 
-  void set_uart(uart::UARTComponent *parent) { this->set_uart_parent(parent); }
   void set_error_sensor(sensor::Sensor *s) { this->error_sensor_ = s; }
 
   void setup() override;
@@ -25,15 +23,11 @@ class MideaDehumComponent : public climate::Climate,
 
  protected:
   void parse_frame_(const std::vector<uint8_t> &frame);
-  void send_cmd3_(uint8_t cmd, uint8_t data);
+  void send_command_(const std::vector<uint8_t> &cmd);
   static uint8_t checksum_(const std::vector<uint8_t> &bytes);
 
-  static uint8_t encode_mode_(climate::ClimateMode m, climate::ClimatePreset preset);
-  static climate::ClimateMode map_mode_for_ui_(uint8_t proto_mode);
-  static uint8_t encode_fan_(climate::ClimateFanMode f);
-  static climate::ClimateFanMode map_fan_for_ui_(uint8_t proto_fan);
+  std::vector<uint8_t> rx_buf_;
 
-  std::vector<uint8_t> rx_;
   sensor::Sensor *error_sensor_{nullptr};
 };
 
