422f56808d2d415029ee6b41570e52012b7b4013
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2025-10-04 11:47:13 +0200

    Update midea_dehum.h

--- components/midea_dehum/midea_dehum.h
+++ components/midea_dehum/midea_dehum.h
@@ -22,11 +22,12 @@ static const char *const PRESET_CLOTHES_DRY = "clothes_dry";
 
 class MideaDehumComponent : public climate::Climate, public Component, public uart::UARTDevice {
  public:
+  MideaDehumComponent() = default;
   virtual ~MideaDehumComponent();
 
-  climate::ClimateTraits traits() override;  
-
   void set_uart(uart::UARTComponent *uart);
+
+  climate::ClimateTraits traits() override;  
 
   void control(const climate::ClimateCall &call) override;
   void setup() override;
