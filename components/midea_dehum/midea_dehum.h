0dd8e32a5ac721f6d298645a22e3d9addfd9b610
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2025-10-04 22:47:23 +0200

    Update midea_dehum.h

--- components/midea_dehum/midea_dehum.h
+++ components/midea_dehum/midea_dehum.h
@@ -67,6 +67,9 @@ class MideaDehumComponent : public climate::Climate, public Component, public ua
   void build_header_(uint8_t msgType, uint8_t agreementVersion, uint8_t payloadLength);
   void send_message_(uint8_t msgType, uint8_t agreementVersion, uint8_t payloadLength, const uint8_t *payload);
 
+  void updateSetStatus(bool power_on, const std::string &preset,
+                     climate::ClimateFanMode fan, uint8_t humidity_setpoint);
+
   void request_status_();
   void send_set_status_();
 
