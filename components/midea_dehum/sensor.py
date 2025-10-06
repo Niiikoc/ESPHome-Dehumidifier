8bb96340fdc5568bb852fc36f7274f6486f4a1d3
Christos Baltatzidis <68458228+Chreece@users.noreply.github.com>
2025-10-06 20:43:36 +0200

    fic

--- components/midea_dehum/sensor.py
+++ components/midea_dehum/sensor.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import UNIT_EMPTY
+from esphome.const import UNIT_EMPTY, ICON_EMPTY
 from . import midea_dehum_ns, CONF_MIDEA_DEHUM_ID
 
 MideaDehum = midea_dehum_ns.class_("MideaDehumComponent", cg.Component)
@@ -11,13 +11,11 @@ CONF_ERROR = "error"
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(sensor.Sensor),
     cv.Required(CONF_MIDEA_DEHUM_ID): cv.use_id(MideaDehum),
-    cv.Required(CONF_ERROR): sensor.sensor_schema(),
-}).extend({
-    CONF_ERROR: {
-        cv.Optional("unit_of_measurement", default=UNIT_EMPTY): cv.string_strict,
-        cv.Optional("icon", default="mdi:alert-outline"): cv.icon,
-        cv.Optional("accuracy_decimals", default=0): cv.int_,
-    }
+    cv.Required(CONF_ERROR): sensor.sensor_schema(
+        unit_of_measurement=UNIT_EMPTY,
+        icon="mdi:alert-outline",
+        accuracy_decimals=0,
+    ),
 })
 
 async def to_code(config):
