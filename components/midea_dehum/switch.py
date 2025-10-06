a1cdab9ce73d5f522ea30582dd8612d6891f274d
Christos Baltatzidis <68458228+Chreece@users.noreply.github.com>
2025-10-06 21:14:14 +0200

    schema

--- components/midea_dehum/switch.py
+++ components/midea_dehum/switch.py
@@ -1,16 +1,19 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import switch
+from esphome.const import CONF_ID, ICON_AIR_PURIFIER
 from . import midea_dehum_ns, CONF_MIDEA_DEHUM_ID
 
+# Define C++ class
+MideaIonSwitch = midea_dehum_ns.class_("MideaIonSwitch", switch.Switch, cg.Component)
 MideaDehum = midea_dehum_ns.class_("MideaDehumComponent", cg.Component)
 
 CONF_IONIZER = "ionizer"
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(switch.Switch),
+    cv.GenerateID(CONF_ID): cv.declare_id(MideaIonSwitch),
     cv.Required(CONF_MIDEA_DEHUM_ID): cv.use_id(MideaDehum),
-    cv.Required(CONF_IONIZER): switch.switch_schema(icon="mdi:air-purifier")
+    cv.Required(CONF_IONIZER): switch.switch_schema(MideaIonSwitch, icon="mdi:air-purifier"),
 })
 
 async def to_code(config):
