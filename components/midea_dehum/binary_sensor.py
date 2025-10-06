4253acdcba93bff4a9573de78d68384c2b053ccb
Christos Baltatzidis <68458228+Chreece@users.noreply.github.com>
2025-10-06 20:40:51 +0200

    icons

--- components/midea_dehum/binary_sensor.py
+++ components/midea_dehum/binary_sensor.py
@@ -1,9 +1,9 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
+from esphome.const import DEVICE_CLASS_PROBLEM
 from . import midea_dehum_ns, CONF_MIDEA_DEHUM_ID
 
-# Reference to the parent C++ component
 MideaDehum = midea_dehum_ns.class_("MideaDehumComponent", cg.Component)
 
 CONF_BUCKET_FULL = "bucket_full"
@@ -11,7 +11,10 @@ CONF_BUCKET_FULL = "bucket_full"
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(binary_sensor.BinarySensor),
     cv.Required(CONF_MIDEA_DEHUM_ID): cv.use_id(MideaDehum),
-    cv.Required(CONF_BUCKET_FULL): binary_sensor.binary_sensor_schema(),
+    cv.Required(CONF_BUCKET_FULL): binary_sensor.binary_sensor_schema(
+        device_class=DEVICE_CLASS_PROBLEM,
+        icon="mdi:cup-water"
+    ),
 })
 
 async def to_code(config):
--- components/midea_dehum/sensor.py
+++ components/midea_dehum/sensor.py
@@ -1,10 +1,9 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import UNIT_EMPTY, ICON_ALERT
+from esphome.const import UNIT_EMPTY
 from . import midea_dehum_ns, CONF_MIDEA_DEHUM_ID
 
-# Reference to the parent component
 MideaDehum = midea_dehum_ns.class_("MideaDehumComponent", cg.Component)
 
 CONF_ERROR = "error"
@@ -12,7 +11,7 @@ CONF_ERROR = "error"
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(sensor.Sensor),
     cv.Required(CONF_MIDEA_DEHUM_ID): cv.use_id(MideaDehum),
-    cv.Required(CONF_ERROR): sensor.sensor_schema(UNIT_EMPTY, ICON_ALERT, 0),
+    cv.Required(CONF_ERROR): sensor.sensor_schema(UNIT_EMPTY, "mdi:alert-outline", 0),
 })
 
 async def to_code(config):
