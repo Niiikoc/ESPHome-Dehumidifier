2153b6b203362936b4eb020194b77861852b14c0
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2025-10-12 10:52:20 +0200

    Revert "Add native humidity support to MideaDehumComponent"

--- components/midea_dehum/switch.py
+++ components/midea_dehum/switch.py
@@ -7,17 +7,14 @@ from . import midea_dehum_ns, CONF_MIDEA_DEHUM_ID
 cg.add_define("USE_MIDEA_DEHUM_SWITCH")
 
 MideaIonSwitch = midea_dehum_ns.class_("MideaIonSwitch", switch.Switch, cg.Component)
-MideaSwingSwitch = midea_dehum_ns.class_("MideaSwingSwitch", switch.Switch, cg.Component)
 MideaDehum = midea_dehum_ns.class_("MideaDehumComponent", cg.Component)
 
 CONF_IONIZER = "ionizer"
-CONF_SWING = "swing"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(CONF_ID): cv.declare_id(MideaIonSwitch),
     cv.Required(CONF_MIDEA_DEHUM_ID): cv.use_id(MideaDehum),
-    cv.Optional(CONF_IONIZER): switch.switch_schema(MideaIonSwitch, icon="mdi:air-purifier"),
-    cv.Optional(CONF_SWING): switch.switch_schema(MideaSwingSwitch, icon="mdi:format-vertical-align-center"), 
+    cv.Required(CONF_IONIZER): switch.switch_schema(MideaIonSwitch, icon="mdi:air-purifier"),
 })
 
 async def to_code(config):
@@ -26,7 +23,3 @@ async def to_code(config):
     if CONF_IONIZER in config:
         sw = await switch.new_switch(config[CONF_IONIZER])
         cg.add(parent.set_ion_switch(sw))
-        
-    if CONF_SWING in config:
-        sw = await switch.new_switch(config[CONF_SWING])
-        cg.add(parent.set_swing_switch(sw))
